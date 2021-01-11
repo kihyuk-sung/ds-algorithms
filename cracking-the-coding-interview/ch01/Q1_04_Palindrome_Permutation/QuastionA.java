830a54d1aa413d7e4d20c56b7a1f855501e8c3a8
K <PizzaCola.K@gmail.com>
2021-01-11 21:06:39 +0900

    feat: CtCI Palindrome_Permutation
    input will be 'a' to 'z'
    make hash table 28 and check only 1 charactor will come odd number.

---
+++ cracking-the-coding-interview/ch01/Q1_04_Palindrome_Permutation/QuastionA.java
@@ -0,0 +1,31 @@
+package ch01.Q1_04_Palindrome_Permutation;
+
+import java.util.Scanner;
+
+public class QuastionA {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toLowerCase();
+        boolean[] touchTable = new boolean[28]; // input will be 'a' to 'z' string
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if('a' <= c && c <= 'z') {
+                int idx = c - 'a';
+                touchTable[idx] = !touchTable[idx];
+            }
+        }
+
+        int moreChane = 0;
+        for (boolean b : touchTable) {
+            if (b) {
+                moreChane++;
+            }
+            if (moreChane > 2) {
+                System.out.println(false);
+                System.exit(0);
+            }
+        }
+        System.out.println(true);
+    }
+}
